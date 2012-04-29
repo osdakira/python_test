7233d36414e70bb9ba5a45582c1fd6a006911f05
Akira Osada <osada@gu3.co.jp>
2012-04-29 16:33:24 +0900

    プロパティテスト

---
+++ property.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+# http://docs.python.org/library/functions.html#property
+
+import unittest
+try:
+    from oktest import test, ok
+except ImportError:
+    import sys
+    print u"Require oktest framework"
+    sys.exit(0)
+
+
+def my_setattr(ins, x):
+    ins._x = x
+
+
+def my_getattr(ins):
+    return ins._x
+
+
+class PropertyOwner(object):
+    x = property(my_getattr, my_setattr)
+
+
+class PropertyTest(unittest.TestCase):
+    @test("propertyでgetとsetが呼べる")
+    def _(self):
+        p = PropertyOwner()
+        p.x = 1
+        ok(p.x) == 1
+
+if __name__ == "__main__":
+    unittest.main()
